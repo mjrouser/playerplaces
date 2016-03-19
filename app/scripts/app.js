7952094f0c6f0124efdb634f3bdc6b012658179e
powpow12 <rob_p_powell@yahoo.com>
2016-03-19 17:01:36 -0400

    built in player data filter

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,7 @@ angular.module('sortApp', [])
 .controller('mainController', function($scope) {
   $scope.sortType     = 'name'; // set the default sort type
   $scope.sortReverse  = false;  // set the default sort order
-  $scope.searchFish   = '';     // set the default search/filter term
+  $scope.searchPlayer   = '';     // set the default search/filter term
   
   // create the list of sushi rolls 
   $scope.player_data = window.player_data;
