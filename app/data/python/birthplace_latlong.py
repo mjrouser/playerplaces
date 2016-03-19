826a999f5562d6a6a1a3a8ad561c4084fc76284d
Ted Lawless <lawlesst@gmail.com>
2016-03-19 16:14:03 -0400

    Add attributes.

--- app/data/python/birthplace_latlong.py
+++ app/data/python/birthplace_latlong.py
@@ -10,7 +10,7 @@ OpenGeoCode state city database: http://www.opengeocode.org/download.php#stateci
 
 Run as:
 
-$ python birthplaces.py Master.csv statecity.csv
+$ python birthplaces.py Master.csv statecity.csv output.csv
 
 Will output a csv with player birth information plus lat and long.
 
@@ -19,7 +19,14 @@ Will output a csv with player birth information plus lat and long.
 
 import sys
 import petl as etl
-from collections import OrderedDict
+
+
+def add_bbreflink(rec):
+    bid = rec['bbrefID']
+    initial = bid[0]
+    return "http://www.baseball-reference.com/players/" + initial + "/" + bid + ".shtml"
+
+
 
 # Load Master.csv from the Lahman database.
 table = etl.fromcsv(sys.argv[1])
@@ -28,11 +35,15 @@ table = etl.fromcsv(sys.argv[1])
 table2 = etl.select(table, lambda rec: rec.birthCountry == 'USA')
 
 # Only use these fields
-table3 = etl.cut(table2, 'birthCity', 'birthState', 'birthYear')
+table3 = etl.cut(table2, 'nameFirst', 'nameLast', 'debut', 'bbrefID', 'weight', 'height', 'finalGame', 'birthCity', 'birthState', 'birthYear')
 
 # Remove null birth city and birth year
 table4 = etl.select(table3, lambda rec: rec.birthCity != "" and rec.birthYear != "")
 
+# Add Baseball Reference URL
+table5 = etl.addfield(table4, 'baseball_ref_url', add_bbreflink)
+# Remove unnecessary bbrefid
+table6 = etl.cutout(table5, "bbrefID")
 
 # Load city,state lat long table.
 city = etl.fromcsv(sys.argv[2])
@@ -40,9 +51,9 @@ city = etl.fromcsv(sys.argv[2])
 city2 = etl.cut(city, "city", "state", "lat", "long")
 
 # Join tables by two keys
-lat_table = etl.leftjoin(table4, city2, lkey=["birthCity", "birthState"], rkey=["city", "state"])
+lat_table = etl.leftjoin(table6, city2, lkey=["birthCity", "birthState"], rkey=["city", "state"])
 
 # Output merged file to csv
-lat_table.tocsv('players_birth_all.csv')
+lat_table.tocsv(sys.argv[3])
 
 
