44c22f27f55510143fcfe775a53bcdd4e9ebd433
mjrouser <matthew@mjrouser.com>
2016-03-21 23:52:38 -0400

    table is fixed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,12 +24,20 @@ angular.module('sortApp', [
 .controller('MainCtrl', function($scope){
     console.log('something fired');
 })
-.controller('TableCtrl', function($scope) {
+.controller('TableCtrl', function($scope, $http) {
+  
+  $scope.player_data = [];
+
+  $http.get('data/players.json')
+       .then(function(res){
+          $scope.player_data = res.data;                
+        });
+
   $scope.sortType = 'name'; // set the default sort type
   $scope.sortReverse = false;  // set the default sort order
   $scope.searchPlayer = '';     // set the default search/filter term
 
   // create the list of sushi rolls
-  $scope.player_data = window.player_data;
+  //$scope.player_data = window.player_data;
 
 });
