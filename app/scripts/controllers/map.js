4d2a8fb3b63e8766c690724139b2131cd79ba726
mjrouser <matthew@mjrouser.com>
2016-03-22 19:21:11 -0400

    updates

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -2,22 +2,5 @@
 
 angular.module('sortApp')
 .controller('MainCtrl', function($scope){
-  /*
-    console.log('something fired');
-        $scope.gotoAbout = function() {
-      // set the location.hash to the id of
-      // the element you wish to scroll to.
-      $location.hash('about');
-      // call $anchorScroll()
-      $anchorScroll();
-    };
-        $scope.gotoHowto = function() {
-      // set the location.hash to the id of
-      // the element you wish to scroll to.
-      $location.hash('howTo');
 
-      // call $anchorScroll()
-      $anchorScroll();
-    };
-*/
 });
