96af2fbe76881461af2e865c8904132f1229b72f
mjrouser <matthew@mjrouser.com>
2016-03-21 23:02:32 -0400

    pandoras box

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,9 +3,28 @@
 // app.js
 'use strict';
 
-angular.module('sortApp', [])
-
-.controller('mainController', function($scope) {
+angular.module('sortApp', [
+        'ngRoute',
+        'angular.filter'
+	])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/map.html',
+        controller: 'MainCtrl'
+      })
+      .when('/table', {
+        templateUrl: 'views/table.html',
+        controller: 'TableCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+.controller('MainCtrl', function($scope){
+    console.log('something fired');
+})
+.controller('TableCtrl', function($scope) {
   $scope.sortType = 'name'; // set the default sort type
   $scope.sortReverse = false;  // set the default sort order
   $scope.searchPlayer = '';     // set the default search/filter term
