208e54966a4ddafcdddf4e11669aaba0e4191ef5
mjrouser <matthew@mjrouser.com>
2016-03-21 21:11:28 -0400

    updated bower packages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,16 @@
+
+
 // app.js
+'use strict';
+
 angular.module('sortApp', [])
 
 .controller('mainController', function($scope) {
-  $scope.sortType     = 'name'; // set the default sort type
-  $scope.sortReverse  = false;  // set the default sort order
-  $scope.searchPlayer   = '';     // set the default search/filter term
-  
-  // create the list of sushi rolls 
+  $scope.sortType = 'name'; // set the default sort type
+  $scope.sortReverse = false;  // set the default sort order
+  $scope.searchPlayer = '';     // set the default search/filter term
+
+  // create the list of sushi rolls
   $scope.player_data = window.player_data;
-  
+
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,8 @@
+'use strict';
+
 console.log('\'Allo \'Allo!');
 
 
 window.onload = function() {
   cartodb.createVis('map', 'https://baseball-hack.cartodb.com/api/v2/viz/4380d8dc-ee10-11e5-bf98-0ecd1babdde5/viz.json');
-}
+};
